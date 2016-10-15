3e8790608e1fc585abcd2d34f354bb56d7fb97c8
Phil DiMarco <plantlife26@gmail.com>
2016-10-15 19:14:30 -0400

    ground work, backbone, nothing works

---
+++ lib/stock_boy.ex
@@ -0,0 +1,2 @@
+defmodule StockBoy do
+end
--- lib/stock_boy/data_boy.ex
+++ lib/stock_boy/data_boy.ex
@@ -1,4 +1,4 @@
-defmodule DataBoy do
+defmodule StockBoy.DataBoy do
   @stock_db %{protocol: "http", hostname: "localhost",database: "stocks", port: 5984}
 
   def stock(name) do
@@ -29,7 +29,15 @@ defmodule DataBoy do
   def init_db() do
     Couchdb.Connector.Storage.storage_up(@stock_db)
   end
+
+#don't know why this is returning a null value .. still not really
+#sure about "key", the last param needed by the function
+  def test do
+    {:ok, result} = Couchdb.Connector.View.document_by_key(@stock_db, "stock_info", "price_date", "SGYP")
+  end
 end
+
+#{:ok, result} = Couchdb.Connector.View.document_by_key(@stock_db, "stock_info", "price_name")
 
 ## :l, :e, :l_fix, :l_cur, :s, :ltt, :lt, :lt_dts, :c, :c_fix, :cp, :cp_fix, :ccol, :pcls_fix, :el, :el_fix, :el_cur, :elt, :ec, :ec_fix, :ecp, :ecp_fix, :eccol, :chg, :div, :yld]
 
@@ -42,3 +50,6 @@ end
 
 ##stocktwits
 #https://api.stocktwits.com/api/2/streams/symbol/SGYP.json
+
+##querying view
+    #{:ok, result} = Couchdb.Connector.View.document_by_key(db_props, "design_name", "view_name", "key")
